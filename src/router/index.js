ae7adc98f5854c32390a63d85ea9b117a8a7653d
andrei3008 <andrei3008@gmail.com>
2021-05-26 18:41:34 +0300

    abc

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/proiecte",
+    name: "Proiecte",
+    // route level code-splitting
+    // this generates a separate chunk (proiecte.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "proiecte" */ "../views/Proiecte.vue"),
+  },
 ];
 
 const router = createRouter({
